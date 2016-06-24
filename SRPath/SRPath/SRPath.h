0c0dc20464c93fb8e566ab4ade5ed5fd186e1f18
Seorenn <hirenn@gmail.com>
2016-06-24 11:21:45 +0900

    Fixed Build Error for iOS Platform

--- SRPath/SRPath/SRPath.h
+++ SRPath/SRPath/SRPath.h
@@ -6,7 +6,7 @@
 //  Copyright © 2016년 Seorenn. All rights reserved.
 //
 
-#import <Cocoa/Cocoa.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for SRPath.
 FOUNDATION_EXPORT double SRPathVersionNumber;
